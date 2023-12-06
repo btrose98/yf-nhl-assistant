52937bcf6a165e6081d45977a479f2c02088d47d
Bradley Rose <rose.bradley9@gmail.com>
2023-12-05 19:14:10 -0500

    Added query to fetch Top Free Agents available

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ The goal of this project is to provide users with the best free agent pickup opt
     - current fantasy roster
     - available players (what lines they play on, number of games remaining for this week, points, etc.)
     - maximized number of games remaining for the week
+
+Notes:
+
+    - The regular yahoo api (https://developer.yahoo.com/fantasysports/guide/) has information regarding
+        *FILTERING* for players. cntrl + f "Filter" subheading
--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-# main.py
-
-import os
-from pathlib import Path
+import csv
 import json
+from pathlib import Path
+
 from yfpy.query import YahooFantasySportsQuery
 
 # Set directory location of private.json for authentication
@@ -12,30 +11,15 @@ auth_dir = project_dir / "auth"
 
 
 def main():
-#   fetch keys and user information
 
-    with open(auth_dir / "private.json", 'r') as f:
-        credentials = json.load(f)
+    consumer_key, consumer_secret = get_credentials()
 
-    consumer_key = credentials["consumer_key"]
-    consumer_secret = credentials["consumer_secret"]
-
-    # Set the season year
     season = 2023
-
-    # Set the game code to 'nhl' for NHL hockey
     game_code = "nhl"
-
-    # Set your specific league ID for NHL
     league_id = "60441"
-
-    # Set your specific team ID within the league
     team_id = "6"
+    chosen_week = 9  # Replace with the current week or the week you want to retrieve
 
-    # Set the desired week
-    chosen_week = 1  # Replace with the current week or the week you want to retrieve
-
-    # Initialize the YahooFantasySportsQuery object with your credentials
     yahoo_query = YahooFantasySportsQuery(
         auth_dir,
         league_id,
@@ -46,12 +30,15 @@ def main():
         consumer_secret=consumer_secret
     )
 
-    # Fetch the team roster for the specified week
-    team_roster = yahoo_query.get_team_roster_by_week(team_id, chosen_week)
+    league_key = yahoo_query.get_league_key()
 
-    # Print out the player names
-    for player in team_roster:
-        print(player)
+
+    get_my_current_team(yahoo_query, team_id, chosen_week)
+
+    get_available_free_agents(yahoo_query, league_key)
+
+
+
 #     fetch current team
 
 # fetch current free agents
@@ -60,6 +47,51 @@ def main():
 
 # display ordered list of players
 
+
+def get_my_current_team(yahoo_query, team_id, chosen_week):
+    # Fetch the team roster for the specified week
+    team_roster = yahoo_query.get_team_roster_by_week(team_id, chosen_week)
+    for player in team_roster:
+        print(player)
+
+    return team_roster
+
+def get_available_free_agents(yahoo_query, league_key):
+    free_agents = yahoo_query.query(
+        f"https://fantasysports.yahooapis.com/fantasy/v2/league/{league_key}/players;status=FA;sort=AR",
+        ["league", "players"]
+    )
+    unavailable_statuses = ["NA", "IR", "IR-LT", "O"]
+    available_free_agents = [
+        player for player in free_agents
+        if getattr(player, "status", None) not in unavailable_statuses
+           and "injury" not in getattr(player, "status_full", "").lower()
+    ]
+
+    # print(f"available free agents: {available_free_agents}")
+
+    # Write the available free agents to a CSV file
+    with open('available_free_agents.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Write the headers
+        writer.writerow(["Player Name", "Player Key", "Status", "Position", "Team"])
+
+        # Write player data
+        for player in available_free_agents:
+            writer.writerow([getattr(player, "name").full, getattr(player, "player_key"),
+                             getattr(player, "status", "N/A"), getattr(player, "primary_position"),
+                             getattr(player, "editorial_team_abbr")])
+
+    print("Available free agents written to available_free_agents.csv")
+
+    return available_free_agents
+
+def get_credentials():
+    with open(auth_dir / "private.json", 'r') as f:
+        credentials = json.load(f)
+    consumer_key = credentials["consumer_key"]
+    consumer_secret = credentials["consumer_secret"]
+    return consumer_key, consumer_secret
 
 if __name__ == "__main__":
     main()
