9bab04487d04d878f725801bdaa2ef8f42fae267
bradley <bradley@scgcanada.com>
2023-12-06 23:05:17 -0500

    Added query to get fantasy weeks

--- src/main.py
+++ src/main.py
@@ -18,6 +18,9 @@ def main():
     league_id = "60441"
     team_id = "6"
     chosen_week = 9  # Replace with the current week or the week you want to retrieve
+    # Not sure what game_id means ... game_id used for some queries
+    game_id = 427  # NHL - 2023
+
 
     yahoo_query = YahooFantasySportsQuery(
         auth_dir,
@@ -31,11 +34,34 @@ def main():
 
     league_key = yahoo_query.get_league_key()
 
+######################################################################
 
-    get_my_current_team(yahoo_query, team_id, chosen_week)
+    # TESTING EXAMPLE QUERIES
 
-    get_available_free_agents(yahoo_query, league_key)
+    # get_my_current_team(yahoo_query, team_id, chosen_week)
+    # get_available_free_agents(yahoo_query, league_key)
+    print(f"scoreboard: \n{yahoo_query.get_league_scoreboard_by_week(chosen_week)}")
 
+######################################################################
+
+def get_league_settings(yahoo_query):
+    league_settings = yahoo_query.get_league_settings()
+    print(f"league settings:\n{league_settings}")
+    return league_settings
+
+def get_list_of_fantasy_weeks(yahoo_query, fantasy_sport_id):
+    """
+        Returns a list of each fantasy week.
+
+        @:param yahoo_query The wrapper object to query from.
+
+        @:param fantasy_sport_id Represents the sport and year (game_id).
+
+        @:return list of GameWeek (contains start date, end date and week #)
+    """
+    game_weeks_by_game_id = yahoo_query.get_game_weeks_by_game_id(fantasy_sport_id)
+    print(f"game_weeks_by_game_id: \n{yahoo_query, game_weeks_by_game_id}")
+    return game_weeks_by_game_id
 
 def get_my_current_team(yahoo_query, team_id, chosen_week):
     team_roster = yahoo_query.get_team_roster_by_week(team_id, chosen_week)
